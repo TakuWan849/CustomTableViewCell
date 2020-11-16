62095d4489a6680aadde9c0cba96d4e5c795bd96
TakuWan849 <wantaku339@gmail.com>
2020-11-16 18:32:19 +0900

    Initial Commit

---
+++ CustomTableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomTableViewCell
+//
+//  Created by 野澤拓己 on 2020/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
