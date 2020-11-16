d2a9751be61f62e120b5af1854df74b71dab4f4c
TakuWan849 <wantaku339@gmail.com>
2020-11-16 18:48:57 +0900

    tabelViewの作成

--- CustomTableViewCell/ViewController.swift
+++ CustomTableViewCell/ViewController.swift
@@ -9,13 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let tableView : UITableView = {
+       let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // カラー系はviewDidLoadで読み込むとうまく実装される
+        tableView.backgroundColor = .clear
+        tableView.separatorColor = UIColor.clear
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
     }
 
+}
 
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
 
